c6e541e70b584fab46da3a9c5bef51f8888e4950
Дмитрий Поздеев <dmitrypozdeyev@yandex.ru>
2024-08-25 16:14:18 +0300

    Поправил обработку нажатий, добавил покупку

--- src/main/java/org/ezhik/eMAuction/events/ClickEvent.java
+++ src/main/java/org/ezhik/eMAuction/events/ClickEvent.java
@@ -10,22 +10,27 @@ public class ClickEvent implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         if (event.getView().getTitle().contains(AhEM.auctionTitle)) {
+            if(event.getCurrentItem().getItemMeta().getDisplayName().contains("Следующая")){
+                if (AhEM.page * 45 + 45 >= AhEM.lots.size()) return;
+                AhEM.page++;
+                event.getView().close();
+                AhEM.openauction((Player) event.getWhoClicked());
+            }
+            if(event.getCurrentItem().getItemMeta().getDisplayName().contains("Предыдущая")){
+                if (AhEM.page == 0) return;
+                AhEM.page--;
+                event.getView().close();
+                AhEM.openauction((Player) event.getWhoClicked());
+            }
+            if (event.getSlot() >= 0 && event.getSlot() < 45) {
+                if (AhEM.lots.size() <= event.getSlot() + AhEM.page * 45) return;
+                AhEM.buy((Player) event.getWhoClicked(), AhEM.lots.get(event.getSlot() + AhEM.page * 45).get("item").clone());
+            }
             event.setCancelled(true);
         }
         if (event.getView().getTitle().contains(AhEM.BuyTitle)) {
             event.setCancelled(true);
         }
-        if(event.getCurrentItem().getItemMeta().getDisplayName().contains("Следующая")){
-            if (AhEM.page * 45 + 45 >= AhEM.lots.size()) return;
-            AhEM.page++;
-            event.getView().close();
-            AhEM.openauction((Player) event.getWhoClicked());
-        }
-        if(event.getCurrentItem().getItemMeta().getDisplayName().contains("Предыдущая")){
-            if (AhEM.page == 0) return;
-            AhEM.page--;
-            event.getView().close();
-            AhEM.openauction((Player) event.getWhoClicked());
-        }
+
     }
 }
